4db7703b1f852c8adcaa2fafe07c874880177d62
Aliaksandr Artsymenia <Aliaksandr_Artsymenia@epam.com>
2019-10-25 16:16:45 +0300

    Task12

--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -13,7 +13,7 @@ public class Utils {
     }
 
     public static void printMatrix(int[][] arr) {
-        System.out.println("Сгенерированная матрица");
+        System.out.println("Двумерный массив:");
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
                 System.out.print(arr[i][j] + "\t");
